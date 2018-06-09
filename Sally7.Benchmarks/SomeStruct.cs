1e03fd997bd3f61b53748e0913474f85cf065ae3
Michael Croes <mycroes@gmail.com>
2018-06-09 23:18:59 +0200

    Add Sally7.Benchmarks / StructFromSpan benchmark
    Add benchmark project with initial benchmark to compare performance of
    Span<TStruct>[0] vs MemoryMarshal.GetReference(Span<TStruct>).

---
+++ Sally7.Benchmarks/SomeStruct.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Sally7.Benchmarks
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct SomeStruct
+    {
+        public int IntValue;
+        public byte ByteValue;
+    }
+}
