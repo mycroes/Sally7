0ee12897e3ed77c7f35e515cda23dee287b22de2
Michael Croes <mycroes@gmail.com>
2018-06-12 23:35:20 +0200

    Add basic value conversion support

---
+++ Sally7/ValueConversion/IValueConverter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Sally7.ValueConversion
+{
+    public interface IValueConverter
+    {
+        int EncodeDataItemValue(in IDataItem dataItem, Span<byte> buffer);
+        void DecodeDataItemValue(in Span<byte> buffer, IDataItem dataItem, int length);
+    }
+}
---
+++ Sally7/ValueConversion/ValueConverterCollection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sally7.ValueConversion
+{
+    internal class DelegatingValueConverter : IValueConverter
+    {
+        private readonly Dictionary<Type, IValueConverter> valueConverters = BuildValueConverters();
+
+        public int EncodeDataItemValue(in IDataItem dataItem, Span<byte> buffer)
+        {
+            return ValueConverter(dataItem.ValueType).EncodeDataItemValue(dataItem, buffer);
+        }
+
+        public void DecodeDataItemValue(in Span<byte> buffer, IDataItem dataItem, int length)
+        {
+            ValueConverter(dataItem.ValueType).DecodeDataItemValue(buffer, dataItem, length);
+        }
+
+        private static Dictionary<Type, IValueConverter> BuildValueConverters()
+        {
+            return new Dictionary<Type, IValueConverter>
+            {
+                {typeof(int), new Int32ValueConverter()},
+                {typeof(short), new Int16ValueConverter()}
+            };
+        }
+
+        private IValueConverter ValueConverter(Type type)
+        {
+            if (valueConverters.TryGetValue(type, out var converter)) return converter;
+
+            throw new ArgumentException($"No converter is known for type {type.FullName}.");
+        }
+    }
+}
