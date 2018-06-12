6db2dcdcf505f919e2c8e63859484694d42d2505
Michael Croes <mycroes@gmail.com>
2018-06-12 23:36:51 +0200

    Add IDataItem and DataBlockDataItem
    Infrastructure to support actual read/write later on.

---
+++ Sally7/DataBlockDataItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Sally7.Protocol;
+using Sally7.Protocol.S7;
+
+namespace Sally7
+{
+    public class DataBlockDataItem<TValue> : IDataItem<TValue>
+    {
+        private Address address;
+        private int startByte;
+        public BigEndianShort DbNumber { get; set; }
+        public int Length { get; set; }
+        public TValue Value { get; set; }
+
+        public int StartByte
+        {
+            get => startByte;
+            set => address.FromStartByteAndBit(startByte = value, 0);
+        }
+
+        Address IDataItem.Address => address;
+        Area IDataItem.Area => Area.DataBlock;
+        Type IDataItem.ValueType => typeof(TValue);
+        VariableType IDataItem.VariableType => VariableType.Byte;
+    }
+}
---
+++ Sally7/IDataItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Sally7.Protocol;
+using Sally7.Protocol.S7;
+
+namespace Sally7
+{
+    public interface IDataItem
+    {
+        Area Area { get; }
+        BigEndianShort DbNumber { get; }
+        Address Address { get; }
+        int Length { get; }
+        VariableType VariableType { get; }
+        Type ValueType { get; }
+    }
+
+    public interface IDataItem<TValue> : IDataItem
+    {
+        TValue Value { get; set; }
+    }
+}
