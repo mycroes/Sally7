a4b82fb5d6c4dfa62f45b799e63817426692a875
Michael Croes <mycroes@gmail.com>
2018-06-13 23:20:44 +0200

    Add basic benchmark for changing types
    Test some methods that can be used to support enum reading/writing.

---
+++ Sally7.Benchmarks/TypeChanging.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+
+namespace Sally7.Benchmarks
+{
+    public class TypeChanging
+    {
+        [Params(1)]
+        public int Value { get; set; }
+
+        [Benchmark(Baseline = true)]
+        public int RawValueReturn()
+        {
+            return Value;
+        }
+
+        [Benchmark]
+        public Numbers CastToEnumReturn()
+        {
+            return (Numbers) Value;
+        }
+
+        [Benchmark]
+        public Numbers UnsafeAsEnumReturn()
+        {
+            int val = Value;
+            return Unsafe.As<int, Numbers>(ref val);
+        }
+
+        [Benchmark]
+        public int UnsafeAsIntReturn()
+        {
+            int val = Value;
+            return Unsafe.As<int, int>(ref val);
+        }
+    }
+}
