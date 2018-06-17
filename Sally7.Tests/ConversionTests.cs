ea7e1123ea798cef39f381815deda116f27b6ea9
Michael Croes <mycroes@gmail.com>
2018-06-17 23:30:06 +0200

    wip: Add ConverterFactory and tests
    The ConverterFactory will be used with a restructure of the DataItem
    class to avoid converter lookup on every DataItem read/write. It should
    also help in performing conversions for derived types such as enums and
    arrays.

---
+++ Sally7.Tests/ConversionTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Sally7.ValueConversion;
+using Xunit;
+
+namespace Sally7.Tests
+{
+    public class ConversionTests
+    {
+        private readonly ConverterFactory factory = new ConverterFactory();
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(-1)]
+        [InlineData((int) short.MinValue)]
+        [InlineData((int) short.MaxValue)]
+        [InlineData(int.MaxValue)]
+        [InlineData(int.MinValue)]
+        public void ConvertToInt(int value)
+        {
+            var bytes = BitConverter.GetBytes(value);
+            if (BitConverter.IsLittleEndian) Array.Reverse(bytes);
+
+            var converter = ConverterFactory.GetFromPlcConverter<int>();
+            var result = converter(bytes, sizeof(int));
+
+            Assert.Equal(value, result);
+        }
+
+        [Theory]
+        [InlineData(0f)]
+        [InlineData(1f)]
+        [InlineData(0.1f)]
+        [InlineData(float.MinValue)]
+        [InlineData(float.MaxValue)]
+        public void ConvertToFloat(float value)
+        {
+            var bytes = BitConverter.GetBytes(value);
+            if (BitConverter.IsLittleEndian) Array.Reverse(bytes);
+
+            var converter = ConverterFactory.GetFromPlcConverter<float>();
+            var result = converter(bytes, sizeof(float));
+
+            Assert.Equal(value, result);
+        }
+
+        [Theory]
+        [InlineData(EnumOfInt.Zero)]
+        [InlineData(EnumOfInt.One)]
+        [InlineData(EnumOfInt.Two)]
+        [InlineData(EnumOfInt.Three)]
+        [InlineData(EnumOfInt.Four)]
+        [InlineData(EnumOfInt.Five)]
+        [InlineData(EnumOfInt.Six)]
+        [InlineData(EnumOfInt.Seven)]
+        [InlineData(EnumOfInt.Eight)]
+        [InlineData(EnumOfInt.Nine)]
+        [InlineData(EnumOfInt.Ten)]
+        public void ConvertToEnumOfInt(EnumOfInt value)
+        {
+            var bytes = BitConverter.GetBytes((int) value);
+            if (BitConverter.IsLittleEndian) Array.Reverse(bytes);
+
+            var converter = ConverterFactory.GetFromPlcConverter<EnumOfInt>();
+            var result = converter(bytes, sizeof(int));
+
+            Assert.Equal(value, result);
+        }
+
+        public enum EnumOfInt
+        {
+            Zero,
+            One,
+            Two,
+            Three,
+            Four,
+            Five,
+            Six,
+            Seven,
+            Eight,
+            Nine,
+            Ten
+        }
+    }
+}
---
+++ Sally7/ValueConversion/ConverterFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Sally7.ValueConversion
+{
+    public delegate int ConvertToS7<TValue>(in TValue value, in int length, in Span<byte> output);
+
+    public delegate TValue ConvertFromS7<out TValue>(in Span<byte> input, in int length);
+
+    internal class ConverterFactory
+    {
+        public static ConvertFromS7<TValue> GetFromPlcConverter<TValue>() => Unsafe.As<ConvertFromS7<TValue>>(GetFromPlcConverter(typeof(TValue)));
+
+        private static Delegate GetFromPlcConverter(Type type)
+        {
+            if (type.IsEnum) type = Enum.GetUnderlyingType(type);
+
+            if (type == typeof(int)) return new ConvertFromS7<int>(ConvertToInt);
+            if (type == typeof(float)) return new ConvertFromS7<float>(ConvertToIntSized<float>);
+
+            throw new NotImplementedException();
+        }
+
+        private static int ConvertToInt(in Span<byte> input, in int length)
+        {
+            return input[0] << 24 | input[1] << 16 | input[2] << 8 | input[3];
+        }
+
+        private static TValue ConvertToIntSized<TValue>(in Span<byte> input, in int length)
+        {
+            var value = ConvertToInt(input, length);
+            return Unsafe.As<int, TValue>(ref value);
+        }
+    }
+}
