d16231fc4b0dd700c96046706a209f608f31ddaf
Michael Croes <mycroes@gmail.com>
2019-05-12 22:42:24 +0200

    Add HeaderErrorClass enum

---
+++ Sally7/Protocol/S7/HeaderErrorClass.cs
@@ -0,0 +1,13 @@
+﻿namespace Sally7.Protocol.S7
+{
+    internal enum HeaderErrorClass : byte
+    {
+        NoError = 0x00,
+        ApplicationRelationShipError = 0x81,
+        ObjectDefiniationError = 0x82,
+        NoResourcesAvailableError = 0x83,
+        ErrorOnServiceProcessing = 0x84,
+        ErrorOnSupplies = 0x85,
+        AccessError = 0x87
+    }
+}
