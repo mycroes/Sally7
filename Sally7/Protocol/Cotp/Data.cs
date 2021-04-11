dccbcddcc9ab506415176482ab99856478f708b7
Michael Croes <mycroes@gmail.com>
2021-04-11 20:31:52 +0200

    Add .Size and comments to the 'Data' class
    Intended as information for developers, since the class is internal.

--- Sally7/Protocol/Cotp/Data.cs
+++ Sally7/Protocol/Cotp/Data.cs
@@ -6,8 +6,34 @@ namespace Sally7.Protocol.Cotp
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     internal struct Data
     {
+        /// <summary>
+        /// The size of the <see cref="Data"/> struct.
+        /// <para>
+        /// The data struct is not part of the PDU data and it's length should not be considered for the maximum PDU
+        /// size negotiated with the PLC.
+        /// </para>
+        /// </summary>
+        public const int Size = sizeof(byte) + // Length
+            sizeof(byte) + // DataIdentifier
+            sizeof(byte); // PduNumberAndEot
+
+        /// <summary>
+        /// The length of the data segment.
+        /// </summary>
         public byte Length;
+
+        /// <summary>
+        /// The identifier for a data PDU.
+        /// </summary>
         public byte DataIdentifier;
+
+        /// <summary>
+        /// The PDU number and end of transmission indication.
+        /// <para>
+        /// The PDU number is stored in the lower 7 bits. The first bit is used to indicate end of transmission.
+        /// S7 PLC's only support single segment PDU's, so the end of transmission bit should always be set.
+        /// </para>
+        /// </summary>
         public byte PduNumberAndEot;
 
         public void Init()
