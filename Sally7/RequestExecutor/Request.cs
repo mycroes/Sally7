25a41bfeb19710966b4c786107351aa0a735fd2c
Günther Foidl <gue@korporal.at>
2021-10-16 22:23:06 +0200

    Move nested types in ConcurrentRequestExecutor to top-level types

---
+++ Sally7/RequestExecutor/Request.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+
+namespace Sally7.RequestExecutor
+{
+    internal sealed class Request : INotifyCompletion
+    {
+        private static readonly Action Sentinel = () => { };
+
+        private Memory<byte> buffer;
+
+        public bool IsCompleted { get; private set; }
+        private int length;
+        private Action? continuation = Sentinel;
+
+        public Memory<byte> Buffer => buffer;
+
+        public void Complete(int length)
+        {
+            this.length = length;
+            IsCompleted = true;
+
+            var prev = continuation ?? Interlocked.CompareExchange(ref continuation, Sentinel, null);
+            prev?.Invoke();
+        }
+
+        public Memory<byte> GetResult()
+        {
+            return buffer.Slice(0, length);
+        }
+
+        public Request GetAwaiter() => this;
+
+        public void OnCompleted(Action continuation)
+        {
+            if (this.continuation == Sentinel ||
+                Interlocked.CompareExchange(ref this.continuation, continuation, null) == Sentinel)
+            {
+                continuation.Invoke();
+            }
+        }
+
+        public void Reset()
+        {
+            continuation = null;
+            IsCompleted = false;
+        }
+
+        public void SetBuffer(Memory<byte> buffer)
+        {
+            this.buffer = buffer;
+        }
+    }
+}
---
+++ Sally7/RequestExecutor/Signal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Sally7.RequestExecutor
+{
+    [DebuggerNonUserCode]
+    [DebuggerDisplay(nameof(NeedToWait) + ": {" + nameof(NeedToWait) + ",nq}")]
+    internal sealed class Signal : IDisposable
+    {
+        private readonly Channel<int> channel = Channel.CreateBounded<int>(1);
+
+        public void Dispose() => channel.Writer.Complete();
+
+        public bool TryInit() => channel.Writer.TryWrite(0);
+
+        public ValueTask<int> WaitAsync() => channel.Reader.ReadAsync();
+
+        public bool TryRelease() => channel.Writer.TryWrite(0);
+
+        private bool NeedToWait => channel.Reader.Count == 0;
+    }
+}
