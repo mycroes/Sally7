53994807b29341df1b7c13b89274bf6b458989eb
Michael Croes <mycroes@gmail.com>
2024-03-12 19:14:28 +0100

    refactor: Extract Signal from ConcurrentRequestExecutor

--- Sally7/RequestExecutor/ConcurrentRequestExecutor.cs
+++ Sally7/RequestExecutor/ConcurrentRequestExecutor.cs
@@ -2,10 +2,8 @@ using System;
 using System.Buffers;
 using System.Diagnostics;
 using System.Net.Sockets;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading;
-using System.Threading.Channels;
 using System.Threading.Tasks;
 using Sally7.Internal;
 
@@ -186,23 +184,6 @@ namespace Sally7.RequestExecutor
             {
                 jobPool.ReturnJobId(jobId);
             }
-        }
-
-        [DebuggerNonUserCode]
-        [DebuggerDisplay(nameof(NeedToWait) + ": {" + nameof(NeedToWait) + ",nq}")]
-        private class Signal : IDisposable
-        {
-            private readonly Channel<int> channel = Channel.CreateBounded<int>(1);
-
-            public void Dispose() => channel.Writer.Complete();
-
-            public bool TryInit() => channel.Writer.TryWrite(0);
-
-            public ValueTask<int> WaitAsync(CancellationToken cancellationToken) => channel.Reader.ReadAsync(cancellationToken);
-
-            public bool TryRelease() => channel.Writer.TryWrite(0);
-
-            private bool NeedToWait => channel.Reader.Count == 0;
         }
     }
 }
