df9fd20d30815dce00fe271304443a46ba9befa0
Michael Croes <mycroes@gmail.com>
2024-03-13 21:34:08 +0100

    chore(DisposableHelper): Forward to ObjectDisposedException.ThrowIf on net7+

--- Sally7/Internal/DisposableHelper.cs
+++ Sally7/Internal/DisposableHelper.cs
@@ -6,8 +6,12 @@ internal static class DisposableHelper
 {
     public static void ThrowIf(bool condition, object instance)
     {
+#if NET7_OR_GREATER
+        ObjectDisposedException.ThrowIf(condition, instance);
+#else
         void Throw() => throw new ObjectDisposedException(instance.GetType().FullName);
 
         if (condition) Throw();
+#endif
     }
 }
